e2cfdbfbdf6c752e2a3a1e3be9ed971cb2cc6f16
Byt3ChkR <ti.akermann@gmail.com>
2021-07-26 10:20:40 +0200

    Add Windows-x64 and Linux-x64 Builds

--- docs/MakeBuild.ps1
+++ docs/MakeBuild.ps1
@@ -1,14 +1,21 @@
 Add-Type -Assembly System.IO.Compression.FileSystem
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
 
-$in = "$pwd/../src/BadScript.Console/bin/Release/net5.0/publish"
-$out = "$pwd/build.zip"
+$inWin = "$pwd/../src/BadScript.Console/bin/Release/net5.0/win-x64/publish"
+$inLinux = "$pwd/../src/BadScript.Console/bin/Release/net5.0/linux-x64/publish"
+$outWin = "$pwd/build-win.zip"
+$outLinux = "$pwd/build-linux.zip"
 
-del $in -Recurse
-del $out
+del $inWin -Recurse
+del $inLinux -Recurse
+del $outWin
+del $outLinux
 
 cd ../src
+dotnet publish -c Release -r win-x64
+dotnet publish -c Release -r linux-x64
 dotnet publish -c Release
 cd ../docs
 
-[System.IO.Compression.ZipFile]::CreateFromDirectory($in, $out)
+[System.IO.Compression.ZipFile]::CreateFromDirectory($inWin, $outWin)
+[System.IO.Compression.ZipFile]::CreateFromDirectory($inLinux, $outLinux)
