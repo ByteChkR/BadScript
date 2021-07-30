bda75c83f520033d3b882fa3c886a8ce1c83f2e4
Byt3ChkR <ti.akermann@gmail.com>
2021-07-30 12:52:05 +0200

    Update Builds Update MakeBuild.ps1

--- docs/MakeBuild.ps1
+++ docs/MakeBuild.ps1
@@ -8,14 +8,17 @@ $outLinux = "$pwd/build-linux.zip"
 
 del $inRoot -Recurse
 
-$makeZip = {
-    param([string]$sourceDir, [string]$destinationFile, [string]$rid, [string]$docsRoot)
-    del $destinationFile
+$buildProject = {
+    param([string]$rid, [string]$docsRoot)
     cd $docsRoot
     cd ../src
     dotnet publish -c Release -r $rid
     cd ../docs
+}
 
+$makeZip = {
+    param([string]$sourceDir, [string]$destinationFile)
+    del $destinationFile
     Add-Type -Assembly System.IO.Compression.FileSystem
     [System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $destinationFile)
 }
@@ -28,11 +31,27 @@ $debugBuild = {
     cd ../docs
 }
 
-Start-Job $makeZip -ArgumentList ($inWin, $outWin, "win-x64", $pwd) -Name Windows-x64
-Start-Job $makeZip -ArgumentList ($inLinux, $outLinux, "linux-x64", $pwd) -Name Linux-x64
-Start-Job $debugBuild -ArgumentList ($pwd) -Name DebugBuild
+echo "Building Windows-x64"
+$jW = Start-Job $buildProject -ArgumentList ("win-x64", $pwd) -Name Windows-x64
 
+Wait-Job $jW
 
-While (Get-Job -State "Running") { Start-Sleep 2 }
+echo "Building Linux-x64"
+$jL = Start-Job $buildProject -ArgumentList ("linux-x64", $pwd) -Name Linux-x64
+
+Wait-Job $jL
+
+echo "Building Debug Build"
+$jD = Start-Job $debugBuild -ArgumentList ($pwd) -Name DebugBuild
+
+Wait-Job $jD
+
+echo "Packing Windows-x64"
+Start-Job $makeZip -ArgumentList ($inWin, $outWin) -Name Zip-Windows-x64
+
+echo "Packing Linux-x64"
+Start-Job $makeZip -ArgumentList ($inLinux, $outLinux) -Name Zip-Linux-x64
+
+Get-Job | Wait-Job
 Get-Job | Receive-Job
 Remove-Job *
