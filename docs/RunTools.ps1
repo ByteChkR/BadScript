6fb944541ddc989a1085d6b9c4908ce844aba100
Byt3ChkR <ti.akermann@gmail.com>
2021-08-12 12:22:33 +0200

    Update Version

--- docs/RunTools.ps1
+++ docs/RunTools.ps1
@@ -1,6 +1,6 @@
 $preBuildDir = "$pwd/tools/"
 $solutionRoot = "$pwd/../src/"
-$consoleProjectPath = "$pwd/../src/BadScript.Console/BadScript.Console.csproj"
+$consoleProjectPath = "$pwd/../../BadScript.Console/src/BadScript.Console.Desktop/BadScript.Console.Desktop.csproj"
 
 echo "Running Pre Build Scripts"
 dotnet run --project $consoleProjectPath $preBuildDir $solutionRoot
