07727191e4ea7db4f69cc9db1b72a8fcb59594c5
Tim Akermann <ti.akermann@gmail.com>
2021-09-30 03:40:39 +0200

    Add Minimal Example Add Calculator Example Add Integration Example Add Simple Console Example

---
+++ examples/BadScript.Examples.Integration/IntegrationExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using BadScript.Common.Types;
+using BadScript.Settings;
+
+namespace BadScript.Examples.Integration
+{
+
+    internal class IntegrationExample
+    {
+
+        #region Private
+
+        private static BSEngine CreateEngine()
+        {
+            Console.WriteLine( "Creating Script Engine" );
+            BSEngineSettings settings = BSEngineSettings.MakeDefault();
+
+            return settings.Build();
+        }
+
+        private static void Main( string[] args )
+        {
+            BSEngine engine = CreateEngine();
+
+            //Using the GreeterWrapper and NameListWrapper
+            string source = @"
+                greeter = args[0]
+                greetingList = args[1]
+
+                foreach name in greetingList //Use for each on NameListWrapper(Implements IEnumerable<ForEachIteration>)
+                {
+                    greeter.Name = name //Set the Name
+                    greeter.Greet() //Call Greet Function
+                }
+";
+
+            Console.Write( "Enter Names(delimit by comma): " );
+            string names = Console.ReadLine();
+            string[] nameArray = names.Split( new[] { ',' }, StringSplitOptions.RemoveEmptyEntries );
+
+            //Prepare Arguments
+            ABSObject[] arguments =
+            {
+                new GreeterWrapper( new Greeter() ), //Add the wrapper object with a new instance of the GreetingObject
+                new NameListWrapper( new NameList( nameArray ) ) //Add the Name List
+            };
+
+            engine.LoadSource( source, arguments );
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ examples/BadScript.Examples.Minimal/MinimalExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using BadScript.Common.Types;
+using BadScript.Settings;
+
+namespace BadScript.Examples.Minimal
+{
+
+    internal class MinimalExample
+    {
+
+        #region Private
+
+        private static BSEngine CreateEngine()
+        {
+            Console.WriteLine( "Creating Script Engine" );
+            BSEngineSettings settings = BSEngineSettings.MakeDefault();
+
+            return settings.Build();
+        }
+
+        private static void Main( string[] args )
+        {
+            BSEngine engine = CreateEngine();
+
+            string source = "return 92253/1337";
+
+            Console.WriteLine( $"Source: {source}" );
+
+            ABSObject returnValue = engine.LoadSource( source );
+
+            Console.WriteLine( $"Output: {returnValue}" );
+        }
+
+        #endregion
+
+    }
+
+}
