7c94176ef1b1fd3c18df268a5bf1f7295599073a
Tim Akermann <ti.akermann@gmail.com>
2021-10-04 18:06:34 +0200

    Add new Examples Fix Console Example Script

---
+++ examples/BadScript.Examples.ScriptInterfaces/ScriptInterfaceExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+using BadScript.Interfaces;
+using BadScript.Reflection;
+using BadScript.Types;
+using BadScript.Types.Implementations;
+
+namespace BadScript.Examples.ScriptInterfaces
+{
+
+    public class MyCustomScriptInterface : ABSScriptInterface
+    {
+
+        public MyCustomScriptInterface(  ) : base("MyCustomInterface")
+        {
+        }
+
+        public override void AddApi( ABSTable root )
+        {
+            root.InsertElement( "AssemblyName", typeof( MyCustomScriptInterface ).Assembly.FullName );
+
+            root.InsertElement(
+                               "GetLoadedAssemblies",
+                               new BSFunction( "function GetLoadedAssemblies", GetLoadedAssemblies, 0 )
+                              );
+
+            //Because we are trying to set an ABSReference as value, we need to make sure that the table does not resolve the reference away when inserting it.
+            root.SetRawElement(
+                               "WindowName",
+                               new BSReflectionReference(
+                                                         () => new BSObject( Console.Title ),
+                                                         x => Console.Title = x.ConvertString()
+                                                        )
+                              );
+        }
+
+        private ABSObject GetLoadedAssemblies( ABSObject[] arg )
+        {
+            return new BSArray(
+                                      AppDomain.CurrentDomain.GetAssemblies().Select( x => new BSObject( x.FullName ) )
+                                     );
+        }
+
+    }
+
+    internal class ScriptInterfaceExample
+    {
+        private static BSEngine CreateEngine()
+        {
+            Console.WriteLine("Creating Script Engine");
+            BSEngineSettings settings = BSEngineSettings.MakeDefault();
+
+            settings.Interfaces.Add( new MyCustomScriptInterface() ); //Adding the custom script interface
+
+            //Optional: Load MyCustomInterface on load. 
+            //  settings.ActiveInterfaces.Add("MyCustomInterface");
+
+            //Optional: Load MyCustomInterface into global table on load.
+            //  settings.ActiveInterfaces.Add("#MyCustomInterface");
+
+
+            return settings.Build();
+        }
+
+
+        private static void Main(string[] args)
+        {
+            string source = @"
+                    Environment.LoadInterface(""MyCustomInterface"")
+                    MyCustomInterface.WindowName = ""Script Interface Example""
+                    return Environment.Debug(MyCustomInterface)
+";
+
+            BSEngine engine = CreateEngine();
+            Console.WriteLine("Script Returned: \n" + engine.LoadSource( source ));
+        }
+    }
+}
---
+++ examples/BadScript.Examples.SimpleCustomExpression/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using BadScript.ConsoleUtils;
+using BadScript.Parser;
+using BadScript.Parser.Expressions;
+using BadScript.Parser.Expressions.Implementations.Block.ForEach;
+using BadScript.Parser.OperatorImplementations;
+using BadScript.Parser.Operators;
+using BadScript.Parser.Operators.Implementations;
+using BadScript.Scopes;
+using BadScript.Types;
+using BadScript.Types.Implementations;
+using BadScript.Types.References;
+
+namespace BadScript.Examples.SimpleCustomExpression
+{
+
+    public class BSRangeEnumerableObject : ABSObject, IEnumerable <IForEachIteration>
+    {
+
+        private readonly int m_Start;
+        private readonly int m_End;
+        public BSRangeEnumerableObject( SourcePosition pos,int start, int end) : base( pos )
+        {
+            m_Start =start;
+            m_End = end;
+
+        }
+
+        public override bool IsNull => false;
+
+        public override bool Equals( ABSObject other )
+        {
+            return other is BSRangeEnumerableObject o && ReferenceEquals(this, o);
+        }
+
+        public override ABSReference GetProperty( string propertyName )
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool HasProperty( string propertyName )
+        {
+            return false;
+        }
+
+        public override ABSObject Invoke( ABSObject[] args )
+        {
+            throw new NotSupportedException();
+        }
+
+        public override string SafeToString( Dictionary < ABSObject, string > doneList )
+        {
+            return "Range Enumerator";
+        }
+
+        public override void SetProperty( string propertyName, ABSObject obj )
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool TryConvertBool( out bool v )
+        {
+            v = false;
+
+            return false;
+        }
+
+        public override bool TryConvertDecimal( out decimal d )
+        {
+            d = 0;
+
+            return false;
+        }
+
+        public override bool TryConvertString( out string v )
+        {
+            v = null;
+
+            return false;
+        }
+
+        public IEnumerator < IForEachIteration > GetEnumerator()
+        {
+            for ( int i = m_Start; i < m_End; i++ )
+            {
+                yield return new ForEachIteration( new BSObject( ( decimal )i ) );
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+
+
+    public class BSRangeCustomOperatorImplementation: ABSOperatorImplementation
+    {
+
+        public BSRangeCustomOperatorImplementation(  ) : base( ".." )
+        {
+        }
+
+        public override bool IsCorrectImplementation( ABSObject[] args )
+        {
+            return args.Length == 2 &&
+                   args[0].TryConvertDecimal( out decimal _ ) &&
+                   args[1].TryConvertDecimal( out decimal _ );
+        }
+
+        protected override ABSObject Execute( ABSObject[] args )
+        {
+            decimal dStart = args[0].ConvertDecimal();
+            decimal dEnd = args[1].ConvertDecimal();
+
+            return new BSRangeEnumerableObject(args[0].Position, (int)dStart, (int)dEnd);
+        }
+
+    }
+
+    internal class SimpleCustomExpressionExample
+    {
+
+
+        private static BSEngine CreateEngine()
+        {
+            Console.WriteLine("Creating Script Engine");
+            BSEngineSettings settings = BSEngineSettings.MakeDefault();
+
+            settings.Interfaces.Add(new BSSystemConsoleInterface()); //Add the Console API so we can write things
+
+            return settings.Build();
+        }
+
+        private static void Main(string[] args)
+        {
+            //Create an entry in the Operator Precedence Table.
+            BSOperatorPreceedenceTable.Set( new BSBinaryOperator( 3, //The Operator precedence
+                                                                  "..", //The Operator that gets matched
+                                                                  "op_Range", //The Signature Name
+                                                                  2 ) //The Argument Count
+                                           );
+
+            BSOperatorImplementationResolver.AddImplementation( null, //Optional Mapping. If set to "op_Range", tables can override this custom operator implementation with their own.
+                                                                new BSRangeCustomOperatorImplementation() //Add the Custom implementation.
+                                                               );
+
+            BSEngine engine = CreateEngine();
+
+            string source = @"
+                foreach i in 0..10 //Using the Custom Range Operator
+                {
+                    Console.WriteLine(i)
+                }
+";
+
+            engine.LoadSource( source );
+
+        }
+    }
+}
