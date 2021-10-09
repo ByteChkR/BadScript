2dc76e15c4948da93bd0b54fffd97e3c72776f42
Tim Akermann <ti.akermann@gmail.com>
2021-10-09 17:25:48 +0200

    Code Cleanup

--- src/BadScript/Profiling/BSProfilerData.cs
+++ src/BadScript/Profiling/BSProfilerData.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
-using BadScript.Exceptions;
 using BadScript.Types;
 
 using Newtonsoft.Json;
@@ -13,19 +11,6 @@ namespace BadScript.Profiling
 
     public static class BSProfilerData
     {
-
-        public static bool EnableProfiler
-        {
-            get => s_EnableProfiler;
-            set
-            {
-                if ( value )
-                    Start();
-                else
-                    End();
-                s_EnableProfiler = value;
-            }
-        }
 
         private class BSSerializedProfilerData
         {
@@ -37,6 +22,73 @@ namespace BadScript.Profiling
             public decimal MaxMillis;
 
         }
+
+        private static readonly Stopwatch m_Stopwatch = new Stopwatch();
+
+        private static readonly Dictionary < string, BSProfiledFunctionData > m_FunctionData =
+            new Dictionary < string, BSProfiledFunctionData >();
+
+        private static bool s_EnableProfiler;
+
+        public static bool EnableProfiler
+        {
+            get => s_EnableProfiler;
+            set
+            {
+                if ( value )
+                {
+                    Start();
+                }
+                else
+                {
+                    End();
+                }
+
+                s_EnableProfiler = value;
+            }
+        }
+
+        #region Unity Event Functions
+
+        public static void Start()
+        {
+            m_Stopwatch.Restart();
+        }
+
+        #endregion
+
+        #region Public
+
+        public static void End()
+        {
+            m_Stopwatch.Stop();
+        }
+
+        public static BSFunctionInvocationData ProfilerFunctionEntry( BSFunction f, ABSObject[] args )
+        {
+            BSProfiledFunctionData fData;
+            string fStr = f.SafeToString();
+
+            if ( m_FunctionData.ContainsKey( fStr ) )
+            {
+                fData = m_FunctionData[fStr];
+            }
+            else
+            {
+                fData = m_FunctionData[fStr] = new BSProfiledFunctionData();
+            }
+
+            BSFunctionInvocationData invData = new BSFunctionInvocationData( f, args, m_Stopwatch.ElapsedTicks );
+            fData.Invocations.Add( invData );
+
+            return invData;
+        }
+
+        public static void ProfilerFunctionLeave( BSFunctionInvocationData f )
+        {
+            f.SetEnd( m_Stopwatch.ElapsedTicks );
+        }
+
         public static string SerializeProfilerData()
         {
             List < BSSerializedProfilerData > data = new List < BSSerializedProfilerData >();
@@ -49,20 +101,23 @@ namespace BadScript.Profiling
 
                 foreach ( BSFunctionInvocationData id in d.Value.Invocations )
                 {
-                    decimal millis = (id.EndTicks - id.StartTicks) / (decimal)TimeSpan.TicksPerMillisecond;
+                    decimal millis = ( id.EndTicks - id.StartTicks ) / ( decimal )TimeSpan.TicksPerMillisecond;
 
                     if ( min > millis )
+                    {
                         min = millis;
+                    }
 
                     if ( max < millis )
+                    {
                         max = millis;
+                    }
 
                     avg += millis;
                 }
 
                 avg /= d.Value.Invocations.Count;
 
-                
                 BSSerializedProfilerData da = new BSSerializedProfilerData
                                               {
                                                   Name = d.Key,
@@ -77,39 +132,8 @@ namespace BadScript.Profiling
 
             return JsonConvert.SerializeObject( data, Formatting.Indented );
         }
-        private static readonly Stopwatch m_Stopwatch = new Stopwatch();
-        private static readonly Dictionary <string, BSProfiledFunctionData > m_FunctionData =
-            new Dictionary <string, BSProfiledFunctionData >();
 
-        private static bool s_EnableProfiler;
-
-        public static void Start() => m_Stopwatch.Restart();
-
-        public static void End() => m_Stopwatch.Stop();
-
-        public static BSFunctionInvocationData ProfilerFunctionEntry( BSFunction f , ABSObject[] args)
-        {
-            BSProfiledFunctionData fData;
-            string fStr = f.SafeToString();
-            if ( m_FunctionData.ContainsKey(fStr) )
-            {
-                fData = m_FunctionData[fStr];
-            }
-            else
-            {
-                fData = m_FunctionData[fStr] = new BSProfiledFunctionData();
-            }
-            
-            BSFunctionInvocationData invData = new BSFunctionInvocationData(f, args, m_Stopwatch.ElapsedTicks);
-            fData.Invocations.Add(invData);
-            return invData;
-        }
-
-        public static void ProfilerFunctionLeave(BSFunctionInvocationData f )
-        {
-            f.SetEnd( m_Stopwatch.ElapsedTicks );
-        }
-
+        #endregion
 
     }
 
