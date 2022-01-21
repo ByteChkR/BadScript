da8846b39d956bff9de7218271a82c7ff7878b28
Tim Akermann <ti.akermann@gmail.com>
2022-01-21 18:16:09 +0100

    Remove BadOS Project

--- BadOS/Publish.ps1
+++
@@ -1,5 +0,0 @@
-cd BadShell
-. bs project make -t publish
-cd ../BadOS.App
-. bs project make -t publish
-cd ..
--- BadOS/Release.ps1
+++
@@ -1,5 +0,0 @@
-cd BadShell
-. bs project make -t release
-cd ../BadOS.App
-. bs project make -t release
-cd ..
